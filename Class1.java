68894b88aac6f40f0c98515a8cad5763c0ac1e51
arulxcraze <arulxcraze@gmail.com>
2025-01-23 15:03:01 +0530

    class and objects

---
+++ Class1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+class Numbers {
+    private int a;
+    private int b;
+
+    public void sum() { System.out.println(a + b); }
+
+    public void sub() { System.out.println(a - b); }
+
+    public static void main(String[] args)
+    {
+
+        Numbers obj = new Numbers();
+
+        obj.a = 1;
+        obj.b = 2;
+
+        obj.sum();
+        obj.sub();
+    }
+}
